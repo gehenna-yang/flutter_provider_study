08ff92620baa1c3e5deccfc38e6b91f13376e144
gehenna-yang <winer0707@naver.com>
2023-01-26 14:34:57 +0900

    user data & auth provider

---
+++ lib/user/provider/auth_provider.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:infrearnclass/user/model/user_model.dart';
+import 'package:infrearnclass/user/provider/user_me_provider.dart';
+
+final authProvider = ChangeNotifierProvider<AuthProvider>((ref) {
+  return AuthProvider(ref: ref);
+});
+
+class AuthProvider extends ChangeNotifier {
+  final Ref ref;
+
+  AuthProvider({
+    required this.ref,
+  }) {
+    ref.listen<UserModelBase?>(userMeProvider, (previous, next) {
+      if(previous != next){
+        notifyListeners();
+      }
+    });
+  }
+
+  // splash screen
+  // 앱 처음 시작시 토큰 존재여부 확인
+  // 토큰에 따른 로그인화면 또는 메인화면으로 전환 판단
+  String? redirectLogic(GoRouterState state) {
+    final UserModelBase? user = ref.read(userMeProvider);
+
+    final logginIn = state.location == '/login';
+
+    // 유저 정보가 없는데 로그인중이라면 그대로 페이지 유지
+    // 만약 로그인중이 아니라면 로그인 페이지 이동
+    if(user == null){
+      return logginIn ? null : '/login';
+    }
+
+    // user가 null 이 아님
+
+    // UserModel
+    // 사용자 정보가 있는 상태면
+    // 로그인 중이거나 현재 위치가 SplashScreen이면 홈으로 이동
+    if(user is UserModel) {
+      return logginIn || state.location == '/splash' ? '/' : null;
+    }
+
+    // UserModelError
+    if(user is UserModelError){
+      return !logginIn ? '/login' : null;
+    }
+
+    return null;
+  }
+}
---
+++ lib/user/repository/user_me_repository.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:infrearnclass/common/const/data.dart';
+import 'package:infrearnclass/common/dio/dio.dart';
+import 'package:infrearnclass/user/model/user_model.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'user_me_repository.g.dart';
+
+final userMeRepositoryProvider = Provider<UserMeRepository>((ref) {
+  final dio = ref.watch(dioProvider);
+
+  return UserMeRepository(dio, baseUrl: 'http://$ip/user/me');
+});
+
+@RestApi()
+abstract class UserMeRepository {
+  factory UserMeRepository(Dio dio, {String baseUrl}) = _UserMeRepository;
+
+  @GET('/')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<UserModel> getMe();
+}
