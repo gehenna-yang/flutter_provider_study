07a354285813895110b95e3f590049d41ea93a21
gehenna-yang <winer0707@naver.com>
2024-03-15 09:34:19 +0900

    project setting update

--- lib/common/const/data.dart
+++ lib/common/const/data.dart
@@ -6,7 +6,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 const ACCESS_TOKEN_KEY = 'ACCESS_TOKEN';
 const REFRESH_TOKEN_KEY = 'REFRESH_TOKEN';
 
-const emulip = '192.168.0.130:3000';
+const emulip = '192.168.45.241:3000';
 const simulip = '127.0.0.1:3000';
 
 final ip = Platform.isAndroid ? emulip : simulip;
--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -1,17 +1,11 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:infrearnclass/common/component/custom_text_form_field.dart';
 import 'package:infrearnclass/common/const/colors.dart';
-import 'package:infrearnclass/common/const/data.dart';
-import 'package:infrearnclass/common/dio/dio.dart';
 import 'package:infrearnclass/common/layout/default_layout.dart';
-import 'package:infrearnclass/common/secure_storage/secure_storage.dart';
-import 'package:infrearnclass/common/view/root_tab.dart';
 import 'package:infrearnclass/user/model/user_model.dart';
 import 'package:infrearnclass/user/provider/user_me_provider.dart';
 
@@ -43,9 +37,9 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                _Title(),
+                const _Title(),
                 const SizedBox(height: 16),
-                _SubTitle(),
+                const _SubTitle(),
                 Image.asset('asset/img/misc/logo.png', width: MediaQuery.of(context).size.width/3*2,),
                 CustomTextFormField(
                   hintText: "이메일을 입력해 주세요",
@@ -69,14 +63,14 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
                   style: ElevatedButton.styleFrom(
                     primary: PRIMARY_COLOR,
                   ),
-                  child: Text('로그인'),
+                  child: const Text('로그인'),
                 ),
                 TextButton(
                   onPressed: () {},
                   style: TextButton.styleFrom(
                     primary: Colors.black,
                   ),
-                  child: Text('회원가입'),
+                  child: const Text('회원가입'),
                 ),
               ],
             ),
@@ -92,7 +86,7 @@ class _Title extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('환영합니다!', style: TextStyle(fontSize: 34, fontWeight: FontWeight.w500, color: Colors.black),);
+    return const Text('환영합니다!', style: TextStyle(fontSize: 34, fontWeight: FontWeight.w500, color: Colors.black),);
   }
 }
 
@@ -101,6 +95,6 @@ class _SubTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('이메일과 비밀번호를 입력해서 로그인 해주세요!\n오늘도 성공적인 주문이 되길 :)', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: BODY_TEXT_COLOR),);
+    return const Text('이메일과 비밀번호를 입력해서 로그인 해주세요!\n오늘도 성공적인 주문이 되길 :)', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: BODY_TEXT_COLOR),);
   }
 }
